98747efc3daf61bd11f06142a1f32b96b497c9a9
uncommon-type <uncommon.type@rambler.ru>
2024-05-10 20:58:22 +0300

    [#7] Add SQL scripts that create and populate client table
    - Define client table with following columns: id, firstName, lastName,
    middleName, dateOfBirth, insuranceNumber, accountNumber, managerFullName,
    status, managerUserId
    - Populate “client” table with data

---
+++ clientdb/client/create-client.sql
@@ -0,0 +1,34 @@
+DROP TABLE IF EXISTS "client";
+
+CREATE TABLE "client" (
+ "id" INTEGER PRIMARY KEY GENERATED ALWAYS AS IDENTITY,
+ "firstName" VARCHAR(85) NOT NULL,
+ "lastName" VARCHAR(85) NOT NULL,
+ "middleName" VARCHAR(85),
+ "dateOfBirth" DATE,
+ "insuranceNumber" BIGINT UNIQUE NOT NULL,
+ "accountNumber" VARCHAR(12) UNIQUE NOT NULL,
+ "managerFullName" VARCHAR(255) NOT NULL,
+ "status" VARCHAR(14) NOT NULL,
+ "managerUserId" INTEGER,
+ FOREIGN KEY ("managerUserId") REFERENCES "user"("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
