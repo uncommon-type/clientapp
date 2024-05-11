afcf9e09f2d2936aee90e9bc09728a1dd0a93817
uncommon-type <uncommon.type@rambler.ru>
2024-05-11 14:04:33 +0300

    [#11] Implement user authentication and JWT token generation
    - Install jsonwebtoken package
    - Extract login credentials
    - Get user from db, based on login credentials
    - If no user is found or password does not match, throw error
    - if user is found, hash password, add salt to it, compare hashed
    password with stored password in db
    - If passwords match, generate JWT token with JWT.sign(). Include user ID
    in token's encrypted part
    - If passwords don't match, throw error
    - Return json object with token and userName

--- clientapi/service/user.js
+++ clientapi/service/user.js
@@ -1,3 +1,33 @@
+import { createHash } from 'crypto';
+import jwt from 'jsonwebtoken';
+
+import { secretKey } from '../middleware/jwt-validator.js';
+import { getUserByLogin } from '../model.js';
+
+const salt = process.env.SALT;
+
 export const login = async (req, res, next) => {
-    res.json({ message: 'ok' });
-}
+    try {
+        const { login, password } = req.body;
+        const user = await getUserByLogin(login);
+
+        if (user) {
+            const hash = createHash('sha256')
+                .update(password + salt)
+                .digest('hex');
+
+            if (hash === user.password) {
+                const token = jwt.sign(
+                    { userId: user.id, },
+                    secretKey,
+                    { expiresIn: '1h' }
+                );
+
+                return res.json({ token, userName: user.fullName });
+            }
+        }
+        throw new Error('Sign in failed')
+    } catch (err) {
+        next(err);
+    }
+};
