f4bc508665ceb99dedf96f93dab482286c2f1701
uncommon-type <uncommon.type@rambler.ru>
2024-05-13 10:18:13 +0300

    [#20] Add initial setup for clientwebsite

---
+++ clientwebsite/postcss.config.js
@@ -0,0 +1,12 @@
+import autoprefixer from 'autoprefixer';
+import postcssEasyImport from 'postcss-easy-import';
+
+export const postcss = {
+    plugins: [
+        autoprefixer,
+        postcssEasyImport
+    ]
+}
+
+
+
---
+++ clientwebsite/vite.config.js
@@ -0,0 +1,23 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+
+import { postcss } from './postcss.config';
+
+export default defineConfig({
+    plugins: [react()],
+    root: 'src',
+    resolve: {
+        alias: {
+            '@screens': path.resolve(__dirname, 'src/screens'),
+            '@helpers': path.resolve(__dirname, 'src/helpers'),
+            '@network': path.resolve(__dirname, 'src/network')
+        },
+    },
+    css: { postcss },
+    publicDir: '../public',
+    build: {
+        outDir: '../dist',
+        emptyOutDir: true,
+    }
+})
