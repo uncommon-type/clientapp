506d050eee6172854969647a58409b5e3cc0dc9d
uncommon-type <uncommon.type@rambler.ru>
2024-05-13 14:52:09 +0300

    [#24] Implement <Input/> component
    - Give <Input/> type, label, className props
    - Collect any additional props into 'otherProps' object and spread
    those props into input

---
+++ clientwebsite/src/screens/common/Inputs/Input/Input.jsx
@@ -0,0 +1,16 @@
+import PropTypes from 'prop-types';
+
+export const Input = ({ type = 'text', label, className = '', ...otherProps }) => (
+    <div className='form-item'>
+        <label htmlFor={label}>
+            {label}
+        </label>
+        <input id={label} type={type} name={label} {...otherProps} className={className} />
+    </div>
+);
+
+Input.propTypes = {
+    type: PropTypes.string.isRequired,
+    label: PropTypes.string.isRequired,
+    className: PropTypes.string,
+};
