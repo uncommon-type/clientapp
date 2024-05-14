6dd4cc528a8113e1a8f67552b06f4f77d221a9d2
uncommon-type <uncommon.type@rambler.ru>
2024-05-14 15:44:01 +0300

    [#30] Add CORS support
    - Add  cors middleware to enable Cross-Origin Resource Sharing in app
    - Configure cors with options object that specifies allowed origin
    - Tell app to use cors middleware with provided configuration for every
    incoming request

--- clientapi/server.js
+++ clientapi/server.js
@@ -1,13 +1,20 @@
 import express from 'express';
 import logger from 'morgan';
+import cors from 'cors';
 
 import { router } from './routes.js';
 import { notFound } from './middleware/error.js';
 import { problemTypes } from './config/problem-types.js';
 import { configureProblemDetailsResponse } from './middleware/problem-details-response.js';
 
+const corsOptions = {
+    origin: process.env.ORIGIN
+};
+
 const app = express();
 const port = 3000;
+
+app.use(cors(corsOptions));
 
 const problemDetailsResponseMiddleware =
     configureProblemDetailsResponse(problemTypes);
