2faab13af0a1c04e6bf00fbe8c9409af315b41fb
uncommon-type <uncommon.type@rambler.ru>
2024-05-14 12:39:56 +0300

    [#27] Manage app's links and URLs in centralised maintainable way
    - Store links and URLs in central location
    - Create endpoints utility object for endpoints that will be used for
    making API calls to server
    - Create links utility object for links intended for navigation within
    app
    - Store URL that interacts with REST APIs as environment variable

---
+++ clientwebsite/src/helpers/constants.js
@@ -0,0 +1,15 @@
+export const BASE_URL = import.meta.env.VITE_BASE_URL;
+
+export const endpoints = {
+    login: `${BASE_URL}/login`,
+    clients: {
+        list: `${BASE_URL}/clients`
+    },
+};
+
+export const links = {
+    login: '/',
+    clients: {
+        list: '/clients',
+    },
+};
