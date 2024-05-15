7a92f1c9903487f4c6d1583c3f02bb2e8eac9e9c
uncommon-type <uncommon.type@rambler.ru>
2024-05-15 16:37:10 +0300

    [#37] Add formatDate function to transform date into more user-friendly format with day, month and year separated by dots

---
+++ clientwebsite/src/helpers/datetime.js
@@ -0,0 +1,8 @@
+export const formatDate = (dateToFormat) => {
+    const date = new Date(dateToFormat);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const year = date.getFullYear();
+
+    return `${day}.${month}.${year}`;
+};
