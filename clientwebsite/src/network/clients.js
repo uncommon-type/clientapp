39bde08ae9841baa3af9f32d0e32e6e6ed14fb80
uncommon-type <uncommon.type@rambler.ru>
2024-05-15 17:46:03 +0300

    [#41] Add updateClient function responsible for updating client data on server, authenticated by provided token

--- clientwebsite/src/network/clients.js
+++ clientwebsite/src/network/clients.js
@@ -1,6 +1,10 @@
 import { endpoints } from '@helpers/constants';
-import { getData } from './network';
+import { getData, updateData } from './network';
 
 export const getClients = async (token) => {
     return await getData(endpoints.clients.list, token);
+};
+
+export const updateClient = async (clientToUpdate, token) => {
+    return await updateData(endpoints.clients.list, clientToUpdate, token);
 };
