81cb72f4fcb63599a7377e15a3e703c9c6113a30
uncommon-type <uncommon.type@rambler.ru>
2024-05-15 16:32:20 +0300

    [#34] Enhance Error Handling in <Error/> Component
    - Retrieve information about encountered error by using useRouteError()
    - Display informative error messages to user
    - Provide 'Go back' button that takes user back to previous location
    (if provided) or 'Signin' page by default

--- clientwebsite/src/screens/common/Error/Error.jsx
+++ clientwebsite/src/screens/common/Error/Error.jsx
@@ -1,3 +1,27 @@
-export const Error = () => (
-    <h1>Error</h1>
-)
+import { useLocation, useRouteError } from 'react-router-dom';
+
+import { links } from '@helpers/constants';
+
+import { Button } from '../Button/Button';
+
+export const Error = () => {
+    const error = useRouteError();
+    const location = useLocation()
+    const from = location.state?.from || links.login;
+
+    return (
+        <main className='flow'>
+            <h1>{error.status}</h1>
+            <div className='flow'>
+                <p>{error.statusText}</p>
+                <Button
+                    to={from}
+                    className='with-icon'
+                    icon='arrow'
+                    label='Вернуться назад'
+                    aria-label='Вернуться назад'
+                />
+            </div>
+        </main>
+    )
+};
