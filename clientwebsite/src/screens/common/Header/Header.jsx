2c4246967f0f8ceb60df4d87201d748ff61e7bca
uncommon-type <uncommon.type@rambler.ru>
2024-05-16 18:33:01 +0300

    [#52] Add styling to <Header/>

--- clientwebsite/src/screens/common/Header/Header.jsx
+++ clientwebsite/src/screens/common/Header/Header.jsx
@@ -1,6 +1,8 @@
 import PropTypes from 'prop-types';
 
-export const Header = ({ children }) => <header>{children}</header>;
+import styles from './header.module.css';
+
+export const Header = ({ children }) => <header className={styles.header}>{children}</header>;
 
 Header.propTypes = {
     children: PropTypes.node.isRequired,
--- clientwebsite/src/screens/common/Header/components/User/User.jsx
+++ clientwebsite/src/screens/common/Header/components/User/User.jsx
@@ -5,8 +5,10 @@ import { UserCircleIcon } from '@heroicons/react/24/outline'
 
 import { links } from '@helpers/constants';
 
+import styles from './user.module.css';
+
 export const User = ({ username }) => (
-    <NavLink to={links.clients.list} aria-label='Мой профиль'>
+    <NavLink to={links.clients.list} aria-label='Мой профиль' className={`with-icon ${styles['user']}`}>
         <UserCircleIcon aria-hidden='true' focusable='false' />
         {username}
     </NavLink>
