2b52a388474abf60c15dcffb4d9aa9509d568def
uncommon-type <uncommon.type@rambler.ru>
2024-05-16 16:39:30 +0300

    [#46] Add input styles

--- clientwebsite/src/screens/common/Inputs/Input/Input.jsx
+++ clientwebsite/src/screens/common/Inputs/Input/Input.jsx
@@ -1,5 +1,7 @@
 import PropTypes from 'prop-types';
 
+import styles from './input.module.css';
+
 export const Input = ({ type = 'text', label, className = '', ...otherProps }) => (
     <div className='form-item'>
         <label htmlFor={label}>
