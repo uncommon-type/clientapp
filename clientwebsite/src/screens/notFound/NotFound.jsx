319f2d3bb6d9255b9b2d1487edd58aca15c856fa
uncommon-type <uncommon.type@rambler.ru>
2024-05-16 12:44:09 +0300

    [#43] Implement Not Found page
    - Inform user that page they're looking for doesn't exist
    - Give user way to go back to where they were before

--- clientwebsite/src/screens/notFound/NotFound.jsx
+++ clientwebsite/src/screens/notFound/NotFound.jsx
@@ -1,1 +1,25 @@
-export const NotFound = () => <h1>Not found</h1>
+import { useLocation } from 'react-router-dom'
+
+import { links } from '@helpers/constants'
+
+import { Button } from '@screens/common/Button/Button'
+
+export const NotFound = () => {
+    const location = useLocation()
+    const from = location.state?.from || `${links.login}`;
+
+    return (
+        <main>
+            <h1>404</h1>
+            <div>
+                <p>Извините, апрашиваемая вами страница не найдена</p>
+                <Button
+                    to={from}
+                    icon='arrow'
+                    label='Вернуться назад'
+                    aria-label='Вернуться назад'
+                />
+            </div>
+        </main>
+    )
+}
