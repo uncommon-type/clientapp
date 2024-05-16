674cf018d10bb6730ddf706128ecb17538407063
uncommon-type <uncommon.type@rambler.ru>
2024-05-16 16:38:44 +0300

    [#45] Add styles to <Alert /> component

--- clientwebsite/src/screens/common/Alert/Alert.jsx
+++ clientwebsite/src/screens/common/Alert/Alert.jsx
@@ -1,8 +1,10 @@
 import PropTypes from 'prop-types';
 import { ExclamationTriangleIcon as AlertIcon } from '@heroicons/react/24/outline';
 
+import styles from './alert.module.css';
+
 export const Alert = ({ message }) => (
-    <figure role='alert'>
+    <figure role='alert' className={`${styles['alert']} with-icon`}>
         <AlertIcon />
         <p>{message}</p>
     </figure>
