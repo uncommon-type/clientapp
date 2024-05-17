8052851ed6d9b45be689f8e62a5c72f48b4c74b7
uncommon-type <uncommon.type@rambler.ru>
2024-05-17 11:17:01 +0300

    [#62] Apply consistent base styling to <Input/> component

--- clientwebsite/src/screens/common/Inputs/Input/Input.jsx
+++ clientwebsite/src/screens/common/Inputs/Input/Input.jsx
@@ -3,12 +3,12 @@ import PropTypes from 'prop-types';
 import styles from './input.module.css';
 
 export const Input = ({ type = 'text', label, className = '', ...otherProps }) => (
-    <div className='form-item'>
+    <div className={styles['form-item']}>
         <label htmlFor={label}>
             {label}
         </label>
-        <input id={label} type={type} name={label} {...otherProps} className={className} />
-    </div>
+        <input id={label} type={type} name={label} {...otherProps} className={styles.input} />
+    </div >
 );
 
 Input.propTypes = {
