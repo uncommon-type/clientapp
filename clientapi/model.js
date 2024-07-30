b077b27acb5d77f4bb35e5257a70c25ade02a22b
uncommon-type <uncommon.type@rambler.ru>
2024-07-30 09:47:49 +0300

    [#69] Specify port number on which PostgreSQL db server is listening for connections

--- clientapi/model.js
+++ clientapi/model.js
@@ -7,6 +7,7 @@ const pool = new Pool({
   host: process.env.DB_HOST,
   user: process.env.DB_USER,
   max: 20,
+  port: process.env.PORT,
   idleTimeoutMillis: 30000,
   connectionTimeoutMillis: 2000,
   password: process.env.DB_PWD,
